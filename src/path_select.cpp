d2b291a6df3dfd5370f17e704fa6761b4da4bdb5
Jan Svrcina <svrc.jan@gmail.com>
2026-03-11 01:49:11 +0100

    path select add iterative selection

--- src/path_select.cpp
+++ src/path_select.cpp
@@ -1,12 +1,14 @@
 #include "path_select.hpp"
 
+#include <queue>
+
 #include "utils/stl_print.hpp"
 
 using namespace std;
 
 
-Path_select::Path_select(const Preprocess& prepr, const GRBEnv& grb_env)
-	: inst(prepr.inst), prepr(prepr), grb_env(grb_env)
+Path_select::Path_select(const Preprocess& prepr)
+	: inst(prepr.inst), prepr(prepr)
 {
 	
 }
@@ -14,31 +16,43 @@ Path_select::Path_select(const Preprocess& prepr, const GRBEnv& grb_env)
 
 Path_select::~Path_select()
 {
-	if (this->op_var != nullptr) {
-		delete this->op_var;
-	}
 
-	if (this->model != nullptr) {
-		delete this->model;
-	}
 }
 
 
 void Path_select::select_paths(vector<vector<int>>& paths, vector<int>& junct_time)
 {
 	this->propagate_junct_time(junct_time);
-	
-	vector<Res_overlap> overlaps;
-	this->get_res_overlaps(overlaps, junct_time);
+	auto op_intervals = this->get_op_intervals(junct_time);
 
 	int n_ops = this->inst.n_ops();
 	int n_trains = this->inst.n_trains();
 	
 	vector<bool> train_selected(n_trains, false);
-	vector<int> op_overlap(n_ops);
+	vector<int> op_cost(n_ops);
 
 	while (true) {
-		
+		int t_max = -1;
+		int cost_max = 0;
+
+		for (int t = 0; t < n_trains; t++) {
+			if (train_selected[t]) {
+				continue;
+			}
+
+			auto& train = this->inst.trains[t];
+
+			int cost = 0;
+
+			for (int o = train.op_start; o < train.op_end(); o++) {
+				cost += op_cost[o];
+			}
+
+			if (cost_max < cost) {
+				t_max = t;
+				cost_max = cost;
+			}
+		}
 	}
 }
 
@@ -47,93 +61,6 @@ void Path_select::select_paths(vector<vector<int>>& paths)
 {
 	vector<int> junct_time(this->prepr.n_juncts(), -1);
 	this->select_paths(paths, junct_time);
-}
-
-
-void Path_select::get_res_overlaps(vector<Res_overlap>& overlaps, 
-	const vector<int>& junct_time)
-{
-	vector<vector<Res_interval>> res_ints;
-	res_ints.resize(this->inst.n_res(), {});
-
-	for (int o = 0; o < this->inst.n_ops(); o++) {
-		auto& i_op = this->inst.ops[o];
-		auto& p_op = this->prepr.ops[o];
-		
-		int op_start = junct_time[p_op.junct_start];
-		int op_end = junct_time[p_op.junct_end];
-
-		for (auto& res : this->inst.ops[o].res) {
-			int start = op_start - i_op.dur;
-			int end = op_end + res.time + i_op.dur;
-			res_ints[res.idx].push_back(Res_interval(i_op.train, o, start, end));
-		}
-	}
-
-	overlaps.clear();
-	for (auto& ri : res_ints) {
-		sort(ri.begin(), ri.end());
-
-		for (int i = 0; i < (int)ri.size(); i++) {
-			auto& a = ri[i];
-			for (int j = i+1; j < (int)ri.size(); j++) {
-				auto& b = ri[j];
-				
-				int size = a.end - b.start;
-
-				if (size > 0 && a.train != b.train) {
-					overlaps.push_back(Res_overlap(a.op, b.op, size));
-				}
-
-				if (size <= 0) {
-					break;
-				}
-			}
-		}
-	}
-
-	sort(overlaps.begin(), overlaps.end());
-
-	int new_size = overlaps.size();
-	int i_max = (int)overlaps.size() - 1;
-	for (int i = 0; i <= i_max; i++) {
-		auto& a = overlaps[i];
-		auto& b = overlaps[i + 1];
-		
-		if (i < i_max && a.op1 == b.op1 && a.op2 == b.op2) {
-			b.size += a.size;
-			a.size = 0;
-
-			new_size -= 1;
-		}
-	}
-
-	vector<Res_overlap> new_overlaps;
-	new_overlaps.reserve(new_size);
-
-	for (auto& ol : overlaps) {
-		if (ol.size > 0) {
-			new_overlaps.push_back(ol);
-		}
-	}
-}
-
-
-void Path_select::add_overlaps_to_model(const vector<Res_overlap>& overlaps)
-{
-	for (auto& ol : overlaps) {
-		if (ol.size == 0) {
-			continue;
-		}
-
-		GRBVar var = this->model->addVar(0.0, GRB_INFINITY, ol.size, GRB_CONTINUOUS);
-		
-		GRBTempConstr tmp_cons = this->op_var[ol.op1] + this->op_var[ol.op2] <= var + 1;
-		GRBConstr cons = this->model->addConstr(tmp_cons);
-		
-		this->res_var.push_back(var);
-		this->res_cons.push_back(cons);
-	}
 }
 
 
@@ -162,42 +89,44 @@ void Path_select::propagate_junct_time(vector<int>& junct_time)
 }
 
 
-void Path_select::extract_paths_from_sol(std::vector<std::vector<int>>& paths)
+vector<Path_select::Interval> Path_select::get_op_intervals(
+	const vector<int>& junct_time)
 {
-	if ((int)paths.size() != this->inst.n_trains()) {
-		paths.resize(this->inst.n_trains(), {});
+	int n_ops = 0;
+	vector<Interval> intervals(n_ops);
+
+	for (int o = 0; o < this->inst.n_ops(); o++) {
+		int j_start = this->prepr.ops[o].junct_start;
+		int j_end = this->prepr.ops[o].junct_end;
+
+		intervals[o] = {junct_time[j_start], junct_time[j_end]};
 	}
 
-	for (int t = 0; t < this->inst.n_trains(); t++) {
-		auto& path = paths[t];
-		auto& train = this->inst.trains[t];
-
-		path.clear();
-		path.push_back(train.op_start);
-
-		while (path.back() != train.op_last()) {
-			int o = path.back();
-			auto& op = this->inst.ops[o];
-
-			int o_select = -1;
-			for (int o_succ : op.succ) {
-				if (this->op_var[o_succ].get(GRB_DoubleAttr_X) > 0.5) {
-					assert(o_select == -1);
-					o_select = o_succ;
-				}
-			}
-
-			assert(o_select > 0);
-			path.push_back(o_select);
-		}
-	}
+	return intervals;
 }
 
-
-
-void Path_select::Res_interval::print(std::ostream& os) const
+struct Prio_op
 {
-	os << "Res_interval(op=" << this->op <<
-		", start=" << this->start <<
-		", end=" << this->end << ")";
+	int op;
+	int prio;
+
+	bool operator<(const Prio_op& other) const { return this->prio > other.prio; }
+};
+
+
+void Path_select::select_train_path(vector<vector<int>>& paths, 
+	const vector<int>& op_cost, int t)
+{
+	auto& path = paths[t];
+	auto& train = this->inst.trains[t];
+	
+	vector<int> op_pred(train.ops.size, -1);
+	vector<int> op_dist(train.ops.size, INT_MAX);
+	op_dist[train.op_start] = 0;
+
+	priority_queue<Prio_op> prio_queue;
+	prio_queue.push({train.op_start, 0});
+
+	
+	
 }
--- src/path_select.hpp
+++ src/path_select.hpp
@@ -12,7 +12,7 @@ public:
 	const Instance& inst;
 	const Preprocess& prepr;
 
-	Path_select(const Preprocess& prepr, const GRBEnv& grb_env);
+	Path_select(const Preprocess& prepr);
 	~Path_select();
 
 	void select_paths(std::vector<std::vector<int>>& paths,
@@ -21,68 +21,25 @@ public:
 	void select_paths(std::vector<std::vector<int>>& paths);
 
 private:
-	struct Res_overlap;
-	struct Res_interval;
+	struct Interval;
 
-	const GRBEnv& grb_env;
-	GRBModel* model = nullptr;
-
-	GRBVar* op_var = nullptr;
-	std::vector<GRBVar> res_var;
-	std::vector<GRBConstr> res_cons;
-
-	void get_res_overlaps(std::vector<Res_overlap>& overlaps,
+	void propagate_junct_time(std::vector<int>& junct_time);
+	std::vector<Interval> get_op_intervals(
 		const std::vector<int>& junct_time);
 
-	void add_overlaps_to_model(const std::vector<Res_overlap>& overlaps);
-	void propagate_junct_time(std::vector<int>& junct_time);
-
-	void extract_paths_from_sol(std::vector<std::vector<int>>& paths);
+	void select_train_path(std::vector<std::vector<int>>& paths, 
+		const std::vector<int>& op_cost, int t);
 };
 
 
-struct Path_select::Res_overlap
+struct Path_select::Interval
 {
-	int op1;
-	int op2;
-	int size;
-
-	Res_overlap(int op1_, int op2_, int size_) 
-		: size(size_)
-	{
-		this->op1 = (op1_ < op2_) ? op1_ : op2_;
-		this->op2 = (op1_ < op2_) ? op2_ : op1_;
-		this->size = size_;
-	}
-
-	bool operator<(const Res_overlap& other) 
-	{
-		return (this->op1 < other.op1) || 
-			((this->op1 == other.op1) && (this->op2 < other.op2));
-	}
-};
-
-
-struct Path_select::Res_interval
-{
-	int train;
-	int op;
 	int start;
 	int end;
 
-	Res_interval(int train_, int op_, int start_, int end_)
-		: train(train_), op(op_), start(start_), end(end_) {}
-
-	bool operator<(const Res_interval& other)
+	bool operator<(const Interval& other)
 	{
-		return (this->start < other.start) || 
+		return (this->start < other.start) ||
 			((this->start == other.start) && (this->end < other.end));
-	}
-
-	void print(std::ostream& os) const;
-	friend std::ostream& operator<<(std::ostream& os, const Res_interval& ri)
-	{
-		ri.print(os);
-		return os;
 	}
 };
