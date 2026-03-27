77137d9ecf255678a36870fe0dd1a5786622aa1e
Jan Svrcina <svrc.jan@gmail.com>
2026-03-27 11:49:43 +0100

    reverse for

--- src/utils/macros.hpp
+++ src/utils/macros.hpp
@@ -3,3 +3,5 @@
 #define METHOD_N(name) inline auto n_##name() const { return this->name.size(); }
 #define METHOD_AFTER(name) inline auto name##_after() const { return this->name##_first + this->name##s.size(); }
 #define METHOD_LAST(name) inline auto name##_last() const { return this->name##_first + this->name##s.size() - 1; }
+
+#define FOR_REVERSE(it, array) for(auto it = array.end() - 1; it >= array.begin(); it--)
